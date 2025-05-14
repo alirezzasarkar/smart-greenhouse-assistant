3fee1f8851784b8cba19a32c4811f3099535c2e5
Alireza Sarkar <alirezasarkar20@gmail.com>
2025-05-14 16:51:41 +0330

    update NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-between bg-gray-700 p-6 min-h-full overflow-hidden">
+    <div className="flex flex-col items-center justify-between pt-30 overflow-hidden">
       <div className="flex flex-col items-center justify-center flex-grow overflow-auto">
         <img
-          src="/public/assets/icons/404.svg"
+          src="/public/assets/icons/notfound.svg"
           alt="404 Page Not Found Graphic"
           className="max-w-full h-auto"
         />
         <p
-          className="text-sm text-gray-100 text-justify px-5 mb-6 leading-relaxed"
+          className="text-sm text-gray-800 text-justify px-5 mt-15 mb-6 leading-relaxed"
           style={{ textAlignLast: "center" }}
         >
           صفحه‌ای که دنبالش می‌گردی، مثل یه گیاه خشک شده ناپدید شده! نگران نباش،
