13d848105acf69ec21b12264e93b1a035461f984
Alireza Sarkar <alirezasarkar20@gmail.com>
2025-05-15 18:31:58 +0330

    update WaterDetection

--- src/pages/WaterDetection.jsx
+++ src/pages/WaterDetection.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import ResultButton from "../components/common/ResultButton";
 import Description from "../components/common/Description";
 import Question from "../components/common/Question";
 import Dropdown from "../components/common/Dropdown";
+import Loader from "../components/common/Loader";
 
 const questions = [
   {
@@ -33,6 +35,27 @@ const handleQuestionChange = (e) => {
 };
 
 const WaterDetection = () => {
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState("");
+  const [selectedPlant, setSelectedPlant] = useState(null);
+  const [error, setError] = useState("");
+
+  const handleResultClick = () => {
+    if (!selectedPlant) {
+      setError("لطفاً یک گیاه انتخاب کنید.");
+      return;
+    }
+
+    setError("");
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setResult(
+        "شناسایی گیاه با موفقیت انجام شد. گیاه شما از خانواده گیاهان آپارتمانی است و نیاز به نور متوسط و آبیاری منظم دارد. این گیاه در شرایط دمایی بین ۱۸ تا ۲۵ درجه سانتی‌گراد بهترین رشد را دارد. همچنین، توصیه می‌شود هر دو هفته یک‌بار از کود مخصوص گیاهان آپارتمانی استفاده کنید. در صورت مشاهده زردی برگ‌ها، ممکن است گیاه شما دچار کمبود مواد مغذی باشد. لطفاً تصویر گیاه خود را بررسی کنید و در صورت نیاز به اطلاعات بیشتر، با کارشناسان ما تماس بگیرید."
+      );
+    }, 5000);
+  };
+
   return (
     <div className="p-4">
       <Description text="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت" />
@@ -40,11 +63,10 @@ const WaterDetection = () => {
         <Dropdown
           options={plantOptions}
           placeholder="نام گیاه"
-          onChange={(selectedOption) =>
-            console.log("Selected plant:", selectedOption)
-          }
+          onChange={(selectedOption) => setSelectedPlant(selectedOption)}
         />
       </div>
+      {error && <p className="text-red-500 text-center">{error}</p>}
       <div className="mt-6">
         {questions.map((q, index) => (
           <Question
@@ -56,7 +78,17 @@ const WaterDetection = () => {
           />
         ))}
       </div>
-      <ResultButton />
+
+      {loading ? <Loader /> : <ResultButton onClick={handleResultClick} />}
+      {result && (
+        <div className="mt-20 text-center">
+          <h3 className="mb-5 text-color">
+            نتایج تشخیص آب و دمای مناسب گیاه شما
+          </h3>
+
+          <p className="text-gray-700 mt-10 text-justify">{result}</p>
+        </div>
+      )}
     </div>
   );
 };
